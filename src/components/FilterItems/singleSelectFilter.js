6d62f6c650587e2c83414775baf53620d5773a25
frodofazii <fazeahmad786@gmail.com>
2021-06-13 16:56:26 +0500

    Fixed custom hook changes and filter has been implemented

--- src/components/FilterItems/singleSelectFilter.js
+++ src/components/FilterItems/singleSelectFilter.js
@@ -1,39 +1,110 @@
-import React, {render} from 'react'
-import styled from 'styled-components';
+import React, { useState, render } from "react";
+import styled from "styled-components";
 
 const Styles = styled.div`
   .footer-links {
     color: white;
   }
-  `
+`;
 
-const SingleSelectFilter = ({keyWord, name}) => {
-    return (
-        <Styles>
-            <div className="filter-item-display filter-item-keyword">
-                <div className="filter-item-wrapper filter-margin">
-                    <a href={`#${keyWord}`} data-toggle="collapse" className="filter-text">{name} -
-                        <span className="filter-text filter-item-ack">someKeyword</span>
-                        <span className="filter-text right-span"><i className="fa fa-chevron-down"></i></span>
-                    </a>
-                    <div id={`${keyWord}`} className="collapse filter-margin">
-                        <form>
-                            <div className="row">
-                                <div className="col-md-8">
-                                    <input type="text" className="form-control form-inline" id="search-keyword"
-                                           placeholder="Enter Keyword"/>
-                                </div>
-                                <div className="col-md-4">
-                                    <button type="submit" className="btn btn-success">Filter</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+const SingleSelectFilter = ({ keyWord, name, horsePayload, setHorseData }) => {
+  const [value, setValue] = useState();
+  const filterSorting = (e) => {
+    e.preventDefault();
+    console.log(value, name);
+    if (name === "Location") {
+      let sortedDistanceArray = horsePayload.filter(
+        (horse) => horse.location == value
+      );
+      setHorseData(sortedDistanceArray);
+    }
+    if (name === "Dam") {
+      let sortedDistanceArray = horsePayload.filter(
+        (horse) => horse.dam == value
+      );
+      setHorseData(sortedDistanceArray);
+    }
+    if (name === "Sire") {
+      let sortedDistanceArray = horsePayload.filter(
+        (horse) => horse.sire == value
+      );
+      setHorseData(sortedDistanceArray);
+    }
+    if (name === "Age") {
+      let sortedDistanceArray = horsePayload.filter(
+        (horse) => horse.age == value
+      );
+      setHorseData(sortedDistanceArray);
+    }
+    if (name === "Sex") {
+      let sortedDistanceArray = horsePayload.filter(
+        (horse) => horse.sex == value
+      );
+      setHorseData(sortedDistanceArray);
+    }
+    if (name === "Price") {
+      let sortedDistanceArray = horsePayload.filter(
+        (horse) => horse.price == value
+      );
+      setHorseData(sortedDistanceArray);
+    }
+    if (name === "Discipline") {
+      let sortedDistanceArray = horsePayload.filter(
+        (horse) => horse.discipline == value
+      );
+      setHorseData(sortedDistanceArray);
+    }
+    if (name === "BreadingCapable") {
+      let sortedDistanceArray = horsePayload.filter(
+        (horse) => horse.breadingCapable == value
+      );
+      setHorseData(sortedDistanceArray);
+    }
+  };
+  return (
+    <Styles>
+      <div className="filter-item-display filter-item-keyword">
+        <div className="filter-item-wrapper filter-margin">
+          <a
+            href={`#${keyWord}`}
+            data-toggle="collapse"
+            className="filter-text"
+          >
+            {name} -
+            <span className="filter-text filter-item-ack">someKeyword</span>
+            <span className="filter-text right-span">
+              <i className="fa fa-chevron-down"></i>
+            </span>
+          </a>
+          <div id={`${keyWord}`} className="collapse filter-margin">
+            <form>
+              <div className="row">
+                <div className="col-md-8">
+                  <input
+                    type="text"
+                    className="form-control form-inline"
+                    id="search-keyword"
+                    placeholder="Enter Keyword"
+                    onChange={(e) => setValue(e.target.value)}
+                  />
                 </div>
-            </div>
-        </Styles>
-    )
-}
+                <div className="col-md-4">
+                  <button
+                    type="submit"
+                    className="btn btn-success"
+                    onClick={(e) => filterSorting(e)}
+                  >
+                    Filter
+                  </button>
+                </div>
+              </div>
+            </form>
+            
+          </div>
+        </div>
+      </div>
+    </Styles>
+  );
+};
 
 export default SingleSelectFilter;
-
--- src/profile/EditListings.js
+++ src/profile/EditListings.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import {Footer} from "../components/footer";
 import horse1 from "../assets/horse1.jpg";
 import {Modal} from "antd";
+import {useInput} from '../input-hook';
+
 
 const Styles = styled.div`
    .remove-button {
@@ -12,6 +14,31 @@ const Styles = styled.div`
 `
 const EditListing = () => {
     const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const {value: name, bind: bindName, reset: resetName} = useInput('');
+    const {value: state, bind: bindState, reset: resetState} = useInput('');
+
+    const {value: email, bind: bindEmail, reset: resetEmail} = useInput('');
+    const {value: zip, bind: bindZip, reset: resetZip} = useInput('');
+
+    const {value: address, bind: bindAddress, reset: resetAddress} = useInput('');
+    const {value: phone, bind: bindPhone, reset: resetPhone} = useInput('');
+
+    const {value: city, bind: bindCity, reset: resetCity} = useInput('');
+    const {value: title, bind: bindTitle, reset: resetTitle} = useInput('');
+
+    const {value: horseName, bind: bindHorseName, reset: resetHorseName} = useInput('');
+    const {value: horseAge, bind: bindHorseAge, reset: resetHorseAge} = useInput('');
+
+    const {value: horseSex, bind: bindHorseSex, reset: resetHorseSex} = useInput('');
+    const {value: horseHeight, bind: bindHorseHeight, reset: resetHorseHeight} = useInput('');
+
+    const {value: horsePrice, bind: bindHorsePrice, reset: resetHorsePrice} = useInput('');
+    const {value: horseColor, bind: bindHorseColor, reset: resetHorseColor} = useInput('');
+
+    const {value: horseDesc, bind: bindHorseDesc, reset: resetHorseDesc} = useInput('');
+
+
 
     const showModal = () => {
         setIsModalVisible(true);
@@ -60,22 +87,22 @@ const EditListing = () => {
 
                         <div className="row">
                             <div className="col-md-6">
-                                <label>Your Name</label>
-                                <input placeholder={"Your name"} className={"form-control"}/>
+                            <label>Your Name</label>
+                                <input {...bindName} placeholder={"Your name"} className={"form-control"}/>
                                 <label>Your Email</label>
-                                <input placeholder={"Your email"} className={"form-control"}/>
+                                <input {...bindEmail} placeholder={"Your email"} className={"form-control"}/>
                                 <label>Your Address</label>
-                                <input placeholder={"Your address"} className={"form-control"}/>
+                                <input {...bindAddress} placeholder={"Your address"} className={"form-control"}/>
                                 <label>City</label>
-                                <input placeholder={"city"} className={"form-control"}/>
+                                <input {...bindCity} placeholder={"city"} className={"form-control"}/>
                             </div>
                             <div className="col-md-6">
-                                <label>State</label>
-                                <input placeholder={"State"} className={"form-control"}/>
+                                 <label>State</label>
+                                <input {...bindState} placeholder={"State"} className={"form-control"}/>
                                 <label>Zip</label>
-                                <input placeholder={"zip"} className={"form-control"}/>
+                                <input {...bindZip} placeholder={"zip"} className={"form-control"}/>
                                 <label>Phone</label>
-                                <input placeholder={"Phone"} className={"form-control"}/>
+                                <input {...bindPhone} placeholder={"Phone"} className={"form-control"}/>
                             </div>
                         </div>
                     </div>
@@ -84,21 +111,21 @@ const EditListing = () => {
                         <div className="text-center">
                             <h2>Horse Information</h2>
                             <p>Listing Title</p>
-                            <input placeholder={"Headline"} className={"form-control w-50 mx-auto"}/>
+                            <input {...bindTitle} placeholder={"Headline"} className={"form-control w-50 mx-auto"}/>
                         </div>
 
                         <div className="row">
                             <div className="col-md-6">
-                                <label>Horse Name</label>
-                                <input placeholder={"Horse name"} className={"form-control"}/>
+                            <label>Horse Name</label>
+                                <input {...bindHorseName} placeholder={"Horse name"} className={"form-control"}/>
                                 <label>Age</label>
-                                <input placeholder={"Age"} className={"form-control"}/>
+                                <input {...bindHorseAge} placeholder={"Age"} className={"form-control"}/>
                                 <label>Sex</label>
-                                <input placeholder={"Sex"} className={"form-control"}/>
+                                <input {...bindHorseSex} placeholder={"Sex"} className={"form-control"}/>
                                 <label>Height</label>
-                                <input placeholder={"Height"} className={"form-control"}/>
+                                <input {...bindHorseHeight} placeholder={"Height"} className={"form-control"}/>
                                 <label>Color</label>
-                                <input placeholder={"Color"} className={"form-control"}/>
+                                <input {...bindHorseColor} placeholder={"Color"} className={"form-control"}/>
                             </div>
                             <div className="col-md-6">
                                 <div className="form-check">
@@ -107,18 +134,16 @@ const EditListing = () => {
                                         Horse is registered
                                     </label>
                                 </div>
-                                <label>Age</label>
-                                <input placeholder={"age"} className={"form-control"}/>
                                 <label>Sex</label>
-                                <input placeholder={"sex"} className={"form-control"}/>
+                                <input {...bindHorseSex} placeholder={"sex"} className={"form-control"}/>
                                 <label>Price</label>
-                                <input placeholder={"price"} className={"form-control"}/>
+                                <input {...bindHorsePrice} placeholder={"price"} className={"form-control"}/>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-md-12">
-                                <label>Horse Description</label>
-                                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <label>Horse Description</label>
+                                <textarea {...bindHorseDesc} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                             </div>
                         </div>
                     </div>
